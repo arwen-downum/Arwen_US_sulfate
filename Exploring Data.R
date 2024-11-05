3b62874e17f045e4f565bfa643f14f6a730369f7
Maria Popescu <mariapopescu100@gmail.com>
2024-11-05 10:01:14 -0500

    .

--- Exploring Data.R
+++ Exploring Data.R
@@ -7,12 +7,35 @@ pacman::p_load(tidyverse, lubridate, akima, reshape2,
 
 chemistry_limno <- chemistry_limno
 
-sulfate <- chemistry_limno|>
-  filter(parameter_name == "so4_mgl")
+sulfate <- chemistry_limno |>
+  mutate(Date = as.Date(sample_date)) |>
+  mutate(Year = year(Date)) |>
+  mutate(DOY = yday(Date)) |>
+  filter(parameter_name == "so4_mgl") |>
+  relocate(Date, Year, DOY, .before = sample_date)
 
-#see how many unique lakes are present
 
-lakes<- sulfate|>
-  distinct(lagoslakeid)
+write.csv(sulfate, "./sulfate_w_dates.csv", row.names = FALSE)
+
+
+#lakes 
+lakes_information<- cluster_information_limno|>
+  select(lagoslakeid, lake_waterarea_ha, lake_states, lake_namelagos)|>
+  mutate(State = lake_states, Lake = lake_namelagos)|>
+  select(-lake_namelagos, -lake_states)
+  
+#join lake information to sulfate
+
+sulfate_w_lakeinfo<- sulfate|>
+  left_join(lakes_information, by = "lagoslakeid")
+
+write.csv(sulfate_w_lakeinfo, "./sulfate_w_lakeinfo.csv")
+
+#see how many records for each year we have
+
+yearly_counts <- sulfate |>
+  mutate(Year = year(sample_date)) |>
+  group_by(Year) |>
+  summarise(Count = n())
 
 #data from 4551 lakes
